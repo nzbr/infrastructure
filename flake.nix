3714f278b3173c653b53878c21a05b703529b315
nzbr <mail@nzbr.de>
2024-08-08 02:16:13 +0200

    add nix shell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          nodejs
+          corepack
+          pulumi
+          pulumictl
+        ];
+      };
+    }
+  );
+}
