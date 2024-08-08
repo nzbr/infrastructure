2f1f0fa49e90ae508cfd374a4119f730dd75c545
nzbr <mail@nzbr.de>
2024-08-09 01:44:09 +0200

    remove old servers

--- pulumi/src/dns/common.ts
+++ pulumi/src/dns/common.ts
@@ -1,14 +1,6 @@
 import {A, AAAA, CAA_BUILDER, CNAME, MX, SRV, TXT} from "../dnscontrol";
 
 export const servers: { [key: string]: { ip4: string; ip6: string } } = {
-  avalanche: {
-    ip4: '202.61.247.0',
-    ip6: '2a03:4000:53:7a::',
-  },
-  storm: {
-    ip4: '45.9.63.165',
-    ip6: '2a03:4000:45:510::',
-  },
   firestorm: {
     ip4: '46.38.250.20',
     ip6: '2a03:4000:b:120::1',
